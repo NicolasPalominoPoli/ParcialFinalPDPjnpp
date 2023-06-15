62356535ec35bdf2c1f782036594089c4cc67c91
Jhoan Palomino <jhoan_palomino82181@elpoli.edu.co>
2023-06-14 20:01:39 -0500

    Ya casi, falta redireccionar al winner

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -44,88 +44,22 @@ const UsersProvider = ({ children }) => {
     const data = await response.json();
     console.log(data.cards[0]);
     setAuxCard(data.cards[0]);
+    console.log(playerOneCards[0].suit);
+    console.log(data.cards[0].suit);
 
+    if(data.cards[0].value == playerOneCards[0].value)
+    {
+      console.log("SON IGUALES LAS LETRAS");
+      if(data.cards[0].suit == 'CLUBS' && playerOneCards[0].suit == 'DIAMONDS' || data.cards[0].suit == 'HEARTS' && playerOneCards[0].suit == 'SPADES')
+      {
+        console.log("SON PINTAS OPUESTAS");
+        setWinner(`El jugador ${users[0].name} ha ganado el juego`);
+        setCount(52);
+      }
+    }
 
-
-
-    // modifyPlayerOneCards(data.cards[0]);
   };
 
-  const modifyPlayerOneCards = (newCardRequest) => {
-    const newCardValue = newCardRequest.value;
-    const newCardSuit = newCardRequest.suit;
-
-    setAuxCard(newCardRequest);
-    console.log(auxCard[0]);
-    console.log("se metio");
-    console.log(newCardValue);
-    console.log(newCardSuit);
-
-    // const auxFilterPlayerOneCardsValue = playerOneCards.filter(
-    //   (c) => c.value === newCardValue
-    // );
-    // console.log("hola");
-    // console.log(auxFilterPlayerOneCardsValue);
-
-    // if (auxFilterPlayerOneCardsValue.length >= 1) {
-    //   const auxFilterPlayerOneCardsSuit = auxFilterPlayerOneCardsValue.filter(
-    //     (c) => c.suit === newCardSuit
-    //   );
-    //   if (auxFilterPlayerOneCardsSuit.length == 0) {
-    //     console.log("No hay cartas de esta pinta, se mete al mazo");
-    //     const auxModifyDifferentCard = playerOneCards.filter(
-    //       (c) => c.value != newCardValue
-    //     );
-    //     console.log("Arreglo de cartas diferentes: ");
-    //     console.log(auxModifyDifferentCard);
-
-    //     const valueCount = {};
-    //     const uniqueCards = [];
-
-    //     for (const card of auxModifyDifferentCard) {
-    //       if (valueCount[card.value]) {
-    //         valueCount[card.value]++;
-    //       } else {
-    //         valueCount[card.value] = 1;
-    //         uniqueCards.push(card);
-    //       }
-    //     }
-
-    //     var filteredCards = uniqueCards.filter(
-    //       (card) => valueCount[card.value] === 1
-    //     );
-
-    //     if (filteredCards.length == 0) {
-    //       filteredCards = uniqueCards.filter(
-    //         (card) => valueCount[card.value] === 2
-    //       );
-    //     }
-    //     if (filteredCards.length != 0) {
-    //       console.log("Cartas filtradas: ");
-    //       console.log(filteredCards);
-
-    //       const codeReplaceCard =
-    //         filteredCards[Math.floor(Math.random() * filteredCards.length)]
-    //           .code;
-    //       console.log(codeReplaceCard);
-
-    //       const newPlayerOneCards = playerOneCards.map((u) =>
-    //         u.code === codeReplaceCard ? newCardRequest : u
-    //       );
-    //       setplayerOneCards(newPlayerOneCards);
-    //     }
-    //     else
-    //     {
-    //         setWinner(`El jugador ${users[0].name} ha ganado el juego`);
-    //         setCount(16);
-    //     }
-    //   } else console.log("Hay cartas de esta pinta, no se mete al mazo");
-    // } else {
-    //   console.log("No hay de este valor, no se mete");
-    // }
-  };
-
-  
 
   const handlerCards = () => {
     if (count < 52) {
