2802228fd70d020a3ab0b319050bc47a8c6633f6
Jhoan Palomino <jhoan_palomino82181@elpoli.edu.co>
2023-06-14 19:48:15 -0500

    Muestra carta a comparar

--- src/components/PlayerDeck.jsx
+++ src/components/PlayerDeck.jsx
@@ -4,16 +4,20 @@ import UsersContext from "../context/UserContext";
 import ImageList from "@mui/material/ImageList";
 
 const PlayerDeck = () => {
-  const { Users, playerOneCards, playerTwoCards } = useContext(UsersContext);
+  const { Users, playerOneCards, auxCard } = useContext(UsersContext);
   return (
     <div>
       <h3>Player {Users.name}</h3>
-      <p>Cards obtained</p>
+      <p>Card obtained</p>
       <ImageList sx={{ width: 1300, height: 164 }} cols={10} rowHeight={164}>
         {playerOneCards.map((card) => (
           
             <Card key={card.code} imagen={card.image} />
         ))}
+      </ImageList>
+      <h3>Carta nueva</h3>
+      <ImageList sx={{ width: 1300, height: 164 }} cols={10} rowHeight={164}>
+            <Card key={auxCard.code} imagen={auxCard.image} />
       </ImageList>
     </div>
   );
